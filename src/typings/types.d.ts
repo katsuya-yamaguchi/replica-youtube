6533caac98abb1472adeb61c1b9696d81b07bf1a
Katsuya003 <bb.win.3@gmail.com>
2019-06-28 05:38:49 +0900

    Delete because it was described in react-app-env.d.ts.

--- src/typings/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { StateType, ActionType } from 'typesafe-actions';
-
-declare module 'MyTypes' {
-  export type RootAction = ActionType<typeof import('../actions/actions')>
-  export type ReducerState = StateType<typeof import('../reducers/reducer').default>
-}
